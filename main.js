4c38dd53c04ebde7af63b7fdc361603c9ccd276e
maxicappe <39876262+maxicappe@users.noreply.github.com>
2022-09-28 06:38:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,119 @@
-const padre_m = document.getElementById('div-dia-hoy-m')
-const limite_m = document.getElementById('div-dia-mañana-m')
+const padre = document.getElementById('ul-to-put-nodes')
 
 
-const funcionAgregar_m = function agg(){
 
-	const resultado_m = document.createElement('h4')
 
-	const resultadoPrompt_m = prompt('Cual fue tu resultado');
-	const text_m = document.createTextNode(resultadoPrompt_m)
 
-	resultado_m.appendChild(text_m)
-	padre_m.appendChild(resultado_m)
+
+
+
+const funcionAgregar_1 = function agg_1(){
+
+	const li_resultado = document.createElement('li')
+	const prompt_resultado = prompt('Cual fue tu resultado');
+	const text = document.createTextNode(prompt_resultado);
+	li_resultado.appendChild(text)
+	padre.appendChild(li_resultado);
+
+	localStorage.setItem( "resultado_1", prompt_resultado );
+
+
 }
 
 
+const resultadoFinal = localStorage.getItem('resultado_1');
+
+	const li_resultado = document.createElement('li')
+	const text = document.createTextNode(resultadoFinal);
+	li_resultado.appendChild(text)
+	padre.appendChild(li_resultado);
 
 
 
 
-const padre_e = document.getElementById('div-dia-hoy-e')
-const limite_e = document.getElementById('div-dia-mañana-e')
 
 
-const funcionAgregar_e = function agg(){
 
-	const resultado_e = document.createElement('h4')
 
-	const resultadoPrompt_e = prompt('Cual fue tu resultado');
-	const text_e = document.createTextNode(resultadoPrompt_e)
 
-	resultado_e.appendChild(text_e)
-	padre_e.appendChild(resultado_e)
+
+const funcionAgregar_2 = function agg_2(){
+
+	const li_resultado_2 = document.createElement('li')
+	const prompt_resultado_2 = prompt('Cual fue tu resultado');
+	const text_2 = document.createTextNode(prompt_resultado_2);
+	li_resultado_2.appendChild(text_2)
+	padre.appendChild(li_resultado_2);
+
+	localStorage.setItem( "resultado_2", prompt_resultado_2 );
+
+
 }
 
+
+const resultadoFinal_2 = localStorage.getItem('resultado_2');
+
+	const li_resultado_2 = document.createElement('li')
+	const text_2 = document.createTextNode(resultadoFinal_2);
+	li_resultado_2.appendChild(text_2)
+	padre.appendChild(li_resultado_2);
+
+
+
+
+
+
+
+
+
+
+const funcionAgregar_3 = function agg_3(){
+
+	const li_resultado_3 = document.createElement('li')
+	const prompt_resultado_3 = prompt('Cual fue tu resultado');
+	const text_3 = document.createTextNode(prompt_resultado_3);
+	li_resultado_3.appendChild(text_3)
+	padre.appendChild(li_resultado_3);
+
+	localStorage.setItem( "resultado_3", prompt_resultado_3 );
+
+	}
+
+
+
+
+const resultadoFinal_3 = localStorage.getItem('resultado_3');
+
+	const li_resultado_3 = document.createElement('li')
+	const text_3 = document.createTextNode(resultadoFinal_3);
+	li_resultado_3.appendChild(text_3)
+	padre.appendChild(li_resultado_3);
+
+
+
+
+
+
+
+
+
+const funcionAgregar_4 = function agg_4(){
+
+	const li_resultado_4 = document.createElement('li')
+	const prompt_resultado_4 = prompt('Cual fue tu resultado');
+	const text_4 = document.createTextNode(prompt_resultado_4);
+	li_resultado_4.appendChild(text_4)
+	padre.appendChild(li_resultado_4);
+
+	localStorage.setItem( "resultado_4", prompt_resultado_4 );
+
+
+}
+
+
+const resultadoFinal_4 = localStorage.getItem('resultado_4');
+
+	const li_resultado_4 = document.createElement('li')
+	const text_4 = document.createTextNode(resultadoFinal_4);
+	li_resultado_4.appendChild(text_4)
+	padre.appendChild(li_resultado_4);
